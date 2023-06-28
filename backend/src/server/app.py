e21d594813d9cbdf746f23aa8a5f6a8f2e993d76
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2023-06-28 11:38:44 +1000

    Added example routes

--- backend/src/server/app.py
+++ backend/src/server/app.py
@@ -1,7 +1,8 @@
 from dotenv import dotenv_values
-from flask import Flask, Blueprint, Response, jsonify
+from flask import Flask, Blueprint, Response, abort, jsonify, request
 from flask_migrate import Migrate
 from os import getenv, path
+from typing import Tuple
 
 from admin.routes import admin_bp
 from charities.routes import charities_bp
@@ -36,10 +37,51 @@ api_bp = Blueprint("api", __name__, url_prefix="/api")
 def index() -> Response:
     return jsonify({"api_version": "1.0.0"})
 
-@api_bp.route("/users/", methods=["GET"])
-def getUsers() -> Response:
-    users = User.query.all()
-    return jsonify(users)
+    
+@api_bp.route("/users", methods=["GET"])
+def getUser() -> Response:
+
+    # Get id given as query string
+    userID = request.args.get('userID')
+
+    if (userID == None):
+        # If no ID was given
+        users = User.query.all()
+        userList = []
+        for user in users:
+            userList.append({"id": user.id,
+                            "email": user.email,
+                            "isAdmin": user.isAdmin})
+        userJson = {"users": userList}
+        return jsonify(userJson)
+    else:
+        # If ID was given
+        user = User.query.get(userID)
+        if (user == None):
+            abort(404, "User of given userID could not be found")
+        return jsonify({
+            "email": user.email,
+            "isAdmin": user.isAdmin
+    })
+
+@api_bp.route("/users/new", methods=["PUT"])
+def newUser() -> Tuple[Response, int]:
+    # Create a new user
+    data = request.get_json()
+    if (not data or 'email' not in data or 'isAdmin' not in data):
+        abort(400, "Bad Request: Missing query parameters")
+
+    email = data['email']
+    isAdmin = data['isAdmin']
+
+    try:
+        isAdmin = int(isAdmin)
+    except:
+        abort(400, "Bad Request: Invalid value for 'isAdmin'")
+    newUser = User(email=email, isAdmin=isAdmin)
+    db.session.add(newUser)
+    db.session.commit()
+    return jsonify({'message': 'User succesfully created'}), 200
 
 
 # Register all blueprints

8146b94396f4e1d9aceafb0d34d4143dd590b44e
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2024-06-22 11:47:34 +1000

    Fixed food service serialisation in some routes

--- backend/src/server/main/routes.py
+++ backend/src/server/main/routes.py
@@ -1,6 +1,7 @@
 import datetime
 from flask import Blueprint, Response, abort, jsonify, request
 from model import Broadcast, FoodService, ServiceType
+from geoalchemy2 import functions as func
 
 ###
 # File containing all the routes relating to operations by generic users.
@@ -50,7 +51,20 @@ def get_restaurants():
         return abort(404, description="Restaurant food service does not exist in the database. Check with system admin.")
 
     food_services: list[FoodService] = FoodService.query.filter_by(type=serviceType.id).all()
-    return jsonify(food_services), 200
+
+    # Serialise the food services
+    response_restaurants = [
+        {
+            'name': food_service.name,
+            'lastContacted': food_service.lastContacted,
+            'notes': food_service.notes,
+            'type': food_service.type,
+            'halalStatus': food_service.halalStatus
+        }
+        for food_service in food_services
+    ]
+
+    return jsonify(response_restaurants), 200
 
 
 @main_bp.route("/restaurant/getNearby", methods=["GET"])
@@ -63,22 +77,39 @@ def get_restaurants_nearby():
     location: The location of the user.
     distance: The distance in meters from the user's location.
     '''
-    distance = request.args.get('distance')
+
+    # Parse args
+    distance = request.args.get('distance', type=float)
     if not distance:
         return abort(400, description="Distance query parameter is required.")
-    #TODO: How to get location of user?
-    # How to calculate distance between two locations?
-    # Should it just be localised to suburbs? coords may not work as db does not force coords to exist
+    user_lat = request.args.get('lat', type=float)
+    user_lon = request.args.get('lon', type=float)
+    if not user_lat or not user_lon:
+        return abort(400, description="Latitude and longitude query parameters are required.")
+    
+    # Get restaurant service type
+    serviceType = ServiceType.query.filter_by(name='Restaurant').first()
+    if not serviceType:
+        return abort(404, description="Restaurant food service does not exist in the database. Check with system admin.")
 
-    # Use maps api (Should put a design pattern here but maybe later)
-    # User inputs coords    # NOTE: I think we run a db update for coords now and enforce it to exist and be added.
-    # Go through db and update coords for each location < --- #TODO: Is this a bad idea? Answer: Yes. Why? Because it's not scalable. What should be done instead? Use a maps api to get coords for each location
-    # Use maps api to get coords for each location < --- # TODO: Is there a better way to do this then go through every restaurant in the db?
-    # Calculate straight line distance between user and each location
-    # Return locations within distance
+    # Query for nearby locations
+    user_point = func.ST_SetSRID(func.ST_MakePoint(user_lon, user_lat), 4326)
+    nearby_locations = FoodService.query.filter(func.ST_DWithin(FoodService.location.geom, user_point, distance)).filter_by(type=serviceType.id).all()
 
- 
-    return jsonify([]), 200
+    # Build response
+    response_locations = [
+        {
+            'name': location.name,
+            'lastContacted': location.lastContacted,
+            'notes': location.notes,
+            'type': location.type,
+            'halalStatus': location.halalStatus
+        }
+        for location in nearby_locations
+    ]
+
+    # Return nearby locations
+    return jsonify(response_locations), 200
 
 
 @main_bp.route("/butcher/getAll", methods=["GET"])
@@ -92,7 +123,19 @@ def get_butchers():
         return abort(404, description="Butcher food service does not exist in the database. Check with system admin.")
 
     food_services: list[FoodService] = FoodService.query.filter_by(type=serviceType.id).all()
-    return jsonify(food_services), 200
+
+    response_butchers = [
+        {
+            'name': food_service.name,
+            'lastContacted': food_service.lastContacted,
+            'notes': food_service.notes,
+            'type': food_service.type,
+            'halalStatus': food_service.halalStatus
+        }
+        for food_service in food_services
+    ]
+
+    return jsonify(response_butchers), 200
 
 
 @main_bp.route("/butcher/getNearby", methods=["GET"])
@@ -105,12 +148,38 @@ def get_butchers_nearby():
     location: The location of the user.
     distance: The distance in meters from the user's location.
     '''
-    distance = request.args.get('distance')
+    # Parse args
+    distance = request.args.get('distance', type=float)
     if not distance:
         return abort(400, description="Distance query parameter is required.")
-    # TODO: Same issue as restaurants
-    # refactor similar code out once done
-    return jsonify([]), 200
+    user_lat = request.args.get('lat', type=float)
+    user_lon = request.args.get('lon', type=float)
+    if not user_lat or not user_lon:
+        return abort(400, description="Latitude and longitude query parameters are required.")
+    
+    # Get restaurant service type
+    serviceType = ServiceType.query.filter_by(name='Butcher').first()
+    if not serviceType:
+        return abort(404, description="Restaurant food service does not exist in the database. Check with system admin.")
+
+    # Query for nearby locations
+    user_point = func.ST_SetSRID(func.ST_MakePoint(user_lon, user_lat), 4326)
+    nearby_locations = FoodService.query.filter(func.ST_DWithin(FoodService.location.geom, user_point, distance)).filter_by(type=serviceType.id).all()
+
+    # Build response
+    response_locations = [
+        {
+            'name': location.name,
+            'lastContacted': location.lastContacted,
+            'notes': location.notes,
+            'type': location.type,
+            'halalStatus': location.halalStatus
+        }
+        for location in nearby_locations
+    ]
+
+    # Return nearby locations
+    return jsonify(response_locations), 200
 
 
 @main_bp.route("/broadcast/getMessages", methods=["GET"])
